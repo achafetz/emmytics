a22125e9a0f09f3e2e44054963f13f807e26925f
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-01-28 13:19:14 -0500

    feat: add client agency, resolves #2

--- R/get_mixpanel_data.R
+++ R/get_mixpanel_data.R
@@ -4,6 +4,7 @@
 #'
 #' @param from_date Start date in 'YYYY-MM-DD' format
 #' @param to_date End date in 'YYYY-MM-DD' format
+#' @param client_agency if provided, will filter down by specific agency (eg la_ldh)
 #' @param cache_dir defaults to working directory 
 #' @param force_reload Logical. If TRUE, bypass cache and fetch fresh data
 #'
@@ -15,16 +16,19 @@
 #' df <- get_mixpanel_data("2025-11-16", "2025-12-19")
 #' }
 
-get_mixpanel_data <- function(from_date, to_date, cache_dir = ".", force_reload = FALSE) {
+get_mixpanel_data <- function(from_date, to_date, client_agency, cache_dir = ".", force_reload = FALSE) {
   
-  # Create base file name (without extension)
-  file_base <- stringr::str_glue("mixpanel_data_{from_date}_to_{to_date}.json")
+  if(missing(client_agency) || is.null(client_agency || client_agency != ""))
+    client_agency <- "all"
+  
+  # Create base file name
+  file_base <- stringr::str_glue("mixpanel_data_{client_agency}_{from_date}_to_{to_date}.json")
   file_path <- file.path(cache_dir, file_base)
   
   # Load cached file if it exists and don't rerun API
   if (file.exists(file_path) && !force_reload) {
-    clean_df <- load_cached_mixpanel(file_name)
-    return(clean_df)
+    df_clean <- load_cached_mixpanel(file_name)
+    return(df_clean)
   }
   
   # Set params for API
@@ -32,6 +36,10 @@ get_mixpanel_data <- function(from_date, to_date, cache_dir = ".", force_reload 
     from_date = from_date,
     to_date = to_date
   )
+  
+  # Add client_agency filter if provided
+  if (!client_agency != "all")
+    params$where <- stringr::str_glue('properties["client_agency_id"]=="{client_agency}"')
   
   #run API
   raw_text <- fetch_mixpanel(params)
