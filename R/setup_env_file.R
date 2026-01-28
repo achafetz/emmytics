ac8457e185788ebfdf20d9904210c2dfccd9c4ce
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-01-28 08:42:39 -0500

    feat: move load env to be with setup env

--- R/setup_env_file.R
+++ R/setup_env_file.R
@@ -1,3 +1,49 @@
+#' Load Environment Variables
+#'
+#' Internal helper to load credentials from .env file.
+#' 
+#' Credentials should be stored in a .env.local file in the project's root 
+#' folder. This function will use dotenv to load the variables from the
+#' .env.local file in as environment variables to be accessed during the API 
+#' call.
+#'
+#' @param env_path path to .env file
+#' @param req_vars required variables, default = c("MIXPANEL_SERVICE_ACCOUNT_USERNAME", "MIXPANEL_SERVICE_ACCOUNT_SECRET", "MIXPANEL_PROJECT_ID")
+#' @return named list of credentials
+#' @keywords internal
+
+load_env <- function(env_path,
+                     req_vars = c("MIXPANEL_SERVICE_ACCOUNT_USERNAME", "MIXPANEL_SERVICE_ACCOUNT_SECRET", "MIXPANEL_PROJECT_ID")){
+  
+  #path to .env.local file
+  if(missing(env_path) || is.null(env_path)){
+    project_root <- dirname(getwd())
+    env_path <- file.path(project_root, ".env.local")
+  }
+  
+  #check if there is a .env.local file
+  if(!file.exists(env_path))
+    cli::cli_abort(c("Cannot find {.file .env.local} file at the expected path",
+                     i = "Expected path = {.file {env_path}}",
+                     i = "Use {.code setup_env()} to create a {.file .env.local} file"
+    ))
+  
+  #load .env.local
+  dotenv::load_dot_env(env_path)
+  
+  #check if all the required variables are there
+  missing_vars <- req_vars[!nzchar(Sys.getenv(req_vars))]
+  
+  if (length(missing_vars) > 0)
+    cli::cli_abort("Missing environment variable{?s}: {.code {paste(missing_vars, collapse = ', ')}}")
+  
+  invisible(TRUE)
+  
+}
+
+
+
+
 #' Setup environment file
 #'
 #' This function creates a .env.local file with API key placeholders if it doesn't
