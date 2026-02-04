54bbd2df5053c8163199d0400f2921bb5037da09
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-02-04 12:51:36 -0500

    doc: add globals

--- R/globals.R
+++ R/globals.R
@@ -1,4 +1,9 @@
 utils::globalVariables(
   c("properties", "timestamp", "insert_id", "file_path", 
     "cbv_flow_id", "client_agency_id", "distinct_id", "end_date", "event", 
-    "name", "pilot", "pilot_pds", "pilot_state", "start_date", "state"))
+    "name", "pilot", "pilot_pds", "pilot_state", "start_date", "state",
+    "dsac_color", "fa_icon", "fill_color", "icon_color", "icon_size",
+    "icon_vjust", "is_primary", "key_events", "key_events_clean", 
+    "key_events_clean_br", "plot_primary", "plot_secondary", "primary_event", 
+    "provider"
+    ))
