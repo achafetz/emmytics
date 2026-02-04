d24dd5379267673a3737152a3b2f73a0cd86c1d2
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-02-04 14:22:30 -0500

    doc: add to global vars

--- R/globals.R
+++ R/globals.R
@@ -5,5 +5,6 @@ utils::globalVariables(
     "dsac_color", "fa_icon", "fill_color", "icon_color", "icon_size",
     "icon_vjust", "is_primary", "key_events", "key_events_clean", 
     "key_events_clean_br", "plot_primary", "plot_secondary", "primary_event", 
-    "provider"
+    "provider", "dsac_gold", "dsac_light_cranberry","dsac_light_navy", 
+    "employer_name", "flow_date", "status"
     ))
