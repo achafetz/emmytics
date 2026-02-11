cd490fbaca8abf50802e72f8b787b8491b7d7630
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-02-11 11:37:44 -0500

    feat: add client agency to pilot_pds

--- R/data.R
+++ R/data.R
@@ -3,10 +3,11 @@
 #' The following dataset notes the pilot periods run in various states. 
 #'
 #' @format ## `pilot_pds`
-#' A data frame with 5 rows and 4 columns
+#' A data frame with 5 rows and 5 columns
 #' \describe{
 #'   \item{name}{Name of the pilot}
 #'   \item{state}{Two letter state abbreviation of the pilot state}
+#'   \item{client_agency}{The state agency code used in Mixpanel}
 #'   \item{start_date, end_date}{start and end period of the pilot, dates}
 #'   \item{pilot}{month and year of pilot}
 #' }
--- R/globals.R
+++ R/globals.R
@@ -1,8 +1,8 @@
 utils::globalVariables(
   c("properties", "timestamp", "insert_id", "file_path", 
     "cbv_flow_id", "client_agency_id", "distinct_id", "end_date", "event", 
-    "name", "pilot", "pilot_pds", "pilot_state", "start_date", "state",
-    "dsac_color", "fa_icon", "fill_color", "icon_color", "icon_size",
+    "name", "pilot", "pilot_pds", "pilot_state", "client_agency","start_date", 
+    "state", "dsac_color", "fa_icon", "fill_color", "icon_color", "icon_size",
     "icon_vjust", "is_primary", "key_events", "key_events_clean", 
     "key_events_clean_br", "plot_primary", "plot_secondary", "primary_event", 
     "provider", "dsac_gold", "dsac_light_cranberry","dsac_light_navy", 
