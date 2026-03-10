d5c87b35f3a1cafdc1a89f80eb254bc7e80340ba
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-03-10 12:06:20 -0400

    fix: cbv_flow needed a min time for display

--- R/follow_applicant.R
+++ R/follow_applicant.R
@@ -40,8 +40,10 @@ follow_applicant <- function(df, applicant, pilot_pd){
   
   #add date to flow and limit columns of interest
   df_viz <- df_viz %>% 
+    dplyr::group_by(cbv_flow_id) %>% 
     dplyr::mutate(flow_date = stringr::str_glue(
-      "CBV Flow: {cbv_flow_id} [{lubridate::as_datetime(timestamp)}]")) %>% 
+      "CBV Flow: {cbv_flow_id} [{lubridate::as_datetime(min(timestamp))}]")) %>% 
+    ungroup() %>% 
     dplyr::distinct(flow_date, timestamp, event, employer_name)
   
   #encode important events
