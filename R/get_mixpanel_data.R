6c407410e5a63d3330f66e265af659f37d099652
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-05-07 09:19:53 -0400

    feat: add in event param to API to limit call, #9

--- R/get_mixpanel_data.R
+++ R/get_mixpanel_data.R
@@ -6,6 +6,9 @@
 #' @param to_date End date in 'YYYY-MM-DD' format
 #' @param client_agency if provided, will filter down by specific agency. Run
 #'  `unique(pilot_pds$client_agency)` to get the set of states/agencies.
+#' @param events Character vector of event names to fetch. If NULL (default),
+#'   all events are returned. E.g. \code{c("ApplicantViewedAgreement", 
+#'   "ApplicantSharedIncomeSummary")} 
 #' @param cache_dir defaults to working directory 
 #' @param force_reload Logical. If TRUE, bypass cache and fetch fresh data
 #'
@@ -15,10 +18,38 @@
 #'
 #' @examples
 #' \dontrun{
-#' df <- get_mixpanel_data("2025-11-16", "2025-12-19")
+#' 
+#' #pull all data over a specific time period
+#' df_all <- get_mixpanel_data("2026-02-14", "2026-03-22")
+#' 
+#' #return just one agency's data over that period
+#' df_la <- get_mixpanel_data("2026-02-14", "2026-03-22", 
+#'                             client_agency = "la_ldh")
+#'                             
+#' #use stored information from pilot_pds
+#' info <- pilot_pds %>%
+#'   filter(state == "LA", pilot == "Feb 2026") %>% 
+#'   select(start_date, end_date, client_agency) %>% 
+#'   mutate(across(c(start_date, end_date), 
+#'                 \(x) as.character(x))) %>% 
+#'     as.list()
+#'   
+#' df_la <- get_mixpanel_data(info$start_date, info$end_date,
+#'                            client_agency = info$client_agency)
+#'                            
+#' #return only specific events
+#' bounding_events <- c("ApplicantViewedAgreement", 
+#'                      "ApplicantSharedIncomeSummary")
+#'                      
+#' df_la <- get_mixpanel_data(info$start_date, info$end_date,
+#'                            client_agency = info$client_agency,
+#'                            events = bounding_events 
+#'                            )
+#' 
 #' }
 
-get_mixpanel_data <- function(from_date, to_date, client_agency, cache_dir = ".", force_reload = FALSE) {
+get_mixpanel_data <- function(from_date, to_date, client_agency, 
+                              events, cache_dir = ".", force_reload = FALSE) {
   
   if(missing(client_agency) || is.null(client_agency) || client_agency == "")
     client_agency <- "all"
@@ -38,6 +69,9 @@ get_mixpanel_data <- function(from_date, to_date, client_agency, cache_dir = "."
     from_date = from_date,
     to_date = to_date
   )
+  
+  if (!missing(events))
+    params$event = jsonlite::toJSON(events, auto_unbox = FALSE)
   
   # Add client_agency filter if provided
   if (client_agency != "all")
